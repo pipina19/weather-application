2cc98f8a8b31b4de110b4f1f58f41330fdd69fe0
pipina19 <67709782+pipina19@users.noreply.github.com>
2020-10-17 15:03:38 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ function formatDate(date) {
   function handleSubmit(event) {
     event.preventDefault();
     let city = document.querySelector("#town-city").value;
-    searchCity(cityInputElement.value);
+    searchCity(city.value);
   }
   
   function searchLocation(position) {
