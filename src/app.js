8edc6860d224be024716122a215b092742657746
pipina19 <67709782+pipina19@users.noreply.github.com>
2020-10-17 18:25:08 +0100

    forecast changes

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ function formatDate(timestamp) {
     let forecastElement = document.querySelector("#forecast");
     
        forecastElement.innerHTML=`
-       
+       <div class="col-5">
             <ul>
               <li>
                 <strong>${formatHours(forecast.dt * 1000)}</strong><br /><span class="weather-forecast-temperature">${Math.round(forecast.main.temp_max)}°/${Math.round(forecast.main.temp_min)}°</span><img
