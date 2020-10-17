1757905907613dcc59f2c5f4f426bfc7f9a6e7e1
pipina19 <67709782+pipina19@users.noreply.github.com>
2020-10-17 16:21:00 +0100

    .

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ function formatDate(date) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt *1000);
-    iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("src" , `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description)
   }
   
