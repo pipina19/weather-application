eb9c4ad69660f4a4481fd1b673c68e2f7aff00a3
pipina19 <67709782+pipina19@users.noreply.github.com>
2020-10-17 18:09:24 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-function formatDate(date) {
+function formatDate(timestamp) {
     let hours = date.getHours();
+    
     if (hours < 10) {
       hours = `0${hours}`;
     }
@@ -17,8 +18,8 @@ function formatDate(date) {
       "Friday",
       "Saturday"
     ];
-    let day = days[dayIndex];
-    return `${day} ${hours}:${minutes}`;
+    let day = days[date.getDay()];
+    return `${day} ${formatHours(timestamp)}`;
   }
   
   function displayWeatherCondition(response) {
@@ -41,11 +42,31 @@ function formatDate(date) {
     iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description)
   }
-  
+  function displayForecast(response){
+    let forecastElement = document.querySelector("#forecast")
+    forecastElement.innerHTML = null;
+    let forecast=null;
+     for (let index=0;index<6;index++){
+       forecast= response.data.list[index];
+       forecastElement.innerHTML+=`
+       <div class="col-5">
+            <ul>
+              <li>
+                <strong>${formatHours(forecast.dt * 1000)}</strong><br /><span class="weather-forecast-temperature">${Math.round(forecast.main.temp_max)}°/${Math.round(forecast.main.temp_min)}°</span><img
+                  src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+                /><br />
+              </li>`
+  }  
+
+
+
   function searchCity(city) {
     let apiKey = "6b8b1bd45ebc6006cd94529030eb9409";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeatherCondition);
+
+    apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
   }
   
   function handleSubmit(event) {
